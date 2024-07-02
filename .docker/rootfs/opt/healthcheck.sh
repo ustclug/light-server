e821f4f08092b7691afbd0b05ef73b9ef61f5cf0
taoky <me@taoky.moe>
2024-07-02 13:18:50 +0800

    Add health check
    Closes: #12

---
+++ .docker/rootfs/opt/healthcheck.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+check_process() {
+  local process_name=$1
+  
+  if pgrep "$process_name" > /dev/null; then
+    return 0
+  else
+    exit 1
+  fi
+}
+
+check_process "squid"
+check_process "nghttpx"
+check_process "crond"
--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ FROM smartentry/alpine:3.18
 MAINTAINER Yifan Gao <docker@yfgao.com>
 
 COPY .docker $ASSETS_DIR
+HEALTHCHECK --interval=30s --timeout=10s --start-period=30s --retries=3 CMD /opt/healthcheck.sh
 
 RUN smartentry.sh build
